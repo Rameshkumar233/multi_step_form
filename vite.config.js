a45328e7d6bc33f16f0bcf974714a5f23746ce82
Ramesh Kumar <test@youremail.com>
2023-11-21 14:04:43 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/multi_step_form/"
-  plugins: [react()],
-})
+    base: "/multi_step_form/",
+    plugins: [react()],
+});
